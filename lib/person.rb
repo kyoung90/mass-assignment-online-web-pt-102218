df5f65c2f49016bbac92c3358bced0afe2eec7ca
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-12-27 18:32:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight
   attr_accessor :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
+    binding.pry
     attributes.each do |attribute, value|
       self.send(("#{attribute}="), value)
     end
